51a59896654db42e160bda959340e56dffe3a1a3
darubis <yennidaruby@gmail.com>
2024-07-19 19:14:12 -0500

    condicionales en java

---
+++ 02-platzi-basic/IfStatement.java
@@ -0,0 +1,16 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = false;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            fileSended++;
+            System.out.println("Archivo enviado");
+        }else{
+            fileSended--;
+            System.out.println("Por favor enciende el bluetooth, para iniciar la transferencia");
+        }
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
+    }
+}
